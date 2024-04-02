8ccbeb53a7d4eb8db48a46de4781ea3f6b66ee65
yongseopK <kk0@kakao.com>
2024-04-02 11:25:43 +0900

    Initial commit

---
+++ src/main/java/com/echomap/cherryblossomclean/CherryBlossomCleanApplication.java
@@ -0,0 +1,12 @@
+package com.echomap.cherryblossomclean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryBlossomCleanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CherryBlossomCleanApplication.class, args);
+  }
+}
---
+++ src/test/java/com/echomap/cherryblossomclean/CherryBlossomCleanApplicationTests.java
@@ -0,0 +1,11 @@
+package com.echomap.cherryblossomclean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherryBlossomCleanApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
